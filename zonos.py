49e2e148291445aa1329a047701b822f3ebafbaa
Peter Todorov <peter.todorov@live.com>
2025-03-04 16:20:25 +0200

    Tet zonos

---
+++ zonos.py
@@ -0,0 +1,18 @@
+import torchaudio
+from zonos.model import Zonos
+from zonos.conditioning import make_cond_dict
+from zonos.utils import DEFAULT_DEVICE as device
+
+# model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-hybrid", device=device)
+model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-transformer", device=device)
+
+wav, sampling_rate = torchaudio.load("/Users/todorov/Downloads/sam_recording_original.m4a")
+speaker = model.make_speaker_embedding(wav, sampling_rate)
+
+cond_dict = make_cond_dict(text="Hello! Let me test how well this works. I'm a bit nervous but excited to see how it goes.", speaker=speaker, language="en-us")
+conditioning = model.prepare_conditioning(cond_dict)
+
+codes = model.generate(conditioning)
+
+wavs = model.autoencoder.decode(codes).cpu()
+torchaudio.save("sample.wav", wavs[0], model.autoencoder.sampling_rate)
